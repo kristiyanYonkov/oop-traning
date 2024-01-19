5bce3c144bb43efbd029e8e6ec2a6d1f9ab20ffb
kristiyanYonkov <todorovk03@gmail.com>
2024-01-19 13:41:09 +0200

    Initial project set up

---
+++ classes/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using classes;
+using Classes;
+using System.Security.Principal;
+/*
+ * Console.WriteLine("Hello, World!");
+
+var accountOne = new BankAccount("Kristiyan", 1500);
+var accountTwo = new BankAccount("Vanko", 10500);
+
+Console.WriteLine($"Account {accountOne.Number} was created for {accountOne.Owner} with {accountOne.Balance} initial ballance.");
+
+Console.WriteLine($"Account {accountTwo.Number} was created for {accountTwo.Owner} with {accountTwo.Balance} initial ballance.");
+
+
+accountOne.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(accountOne.Balance);
+accountOne.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(accountOne.Balance);
+
+Console.WriteLine(accountOne.GetAccountHistory());
+
+
+var accountThree = new BankAccount("Pencho", 50);
+Console.WriteLine($"Account {accountThree.Number} was created for {accountThree.Owner} with {accountThree.Balance} initial ballance.");
+
+accountThree.MakeDeposit(5000, DateTime.Now, "Paycheck");
+accountThree.MakeDeposit(500, DateTime.Now, "rent");
+accountThree.MakeWithdrawal(1500, DateTime.Now, "Car insurance");
+Console.WriteLine(accountThree.Balance);
+Console.WriteLine(accountThree.GetAccountHistory());
+*/
+
+Animal whale = new Animal("mamaml", "Blue Whale", 56);
+whale.DisplayAnimalInfo();
+
+whale.ChangeKing("Sea Creatures");
+whale.DisplayAnimalInfo();
+
+Mammals dog = new Mammals("mamal", "Dog Bingo", 9, "garden house");
+dog.DisplayAnimalInfo();
+
+
